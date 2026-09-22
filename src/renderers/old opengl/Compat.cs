05bd6ef8d8298d99efcee387efb0cc3e0744db14
Marin-MK <m3rein@gmail.com>
2026-09-22 18:38:42 +0200

    Update architecture diagram and remove legacy OpenGL renderer

--- src/renderers/old opengl/Compat.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace odl3d
-{
-    public enum BufferTarget{ArrayBuffer,ElementBuffer}
-    public class Buffer{public uint Handle;}
-    public class VertexArray{public uint Handle;}
-}
